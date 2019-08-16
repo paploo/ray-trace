11e018ebb75b1a66972e479a512e51b6b318c5b9
Jeff Reinecke <jeff@paploo.net>
2019-08-15 20:42:50 -0700

    WIP: Adding tests for intersection edge cases and forgot to implement something meaningful for them. Will likely need to change the type.

--- test/Engine/GeomSpec.hs
+++ test/Engine/GeomSpec.hs
@@ -18,13 +18,13 @@ spec = do
 
         describe "creation and access" $ do
 
-            it "should create from radians" $ 
+            it "should create from radians" $
                 radians (Angle 3.14) `shouldBe` 3.14
 
-            it "should create from degrees" $ 
+            it "should create from degrees" $
                 radians (fromDegrees 180) `shouldBe` pi
 
-            it "should access as degrees" $ 
+            it "should access as degrees" $
                 degrees (Angle pi) `shouldBe` 180.0
 
 
@@ -54,7 +54,7 @@ spec = do
                 point2 = parameterized line2 v
                 in do
                     u `shouldBeAbout` 2.0
-                    v `shouldBeAbout` (-2.0)
+                    v `shouldBeAbout` 2.0
                     point1 `ptShouldBeAbout` point2
 
 
@@ -64,11 +64,28 @@ spec = do
                 expected = Point (3 + sqrt 3, 4+1)
                 in
                   result `ptShouldBeAbout` expected
-                  
+
 
             it "should calculate the intersection of two lines" $ let
                 line1 = Line (Point (-1, 10)) (Angle (pi/3))
                 line2 = Line (Point (1, 10)) (Angle (pi - pi/3))
                 p = intersection line1 line2
                 in
-                    p `ptShouldBeAbout` Point (2, 10 - sqrt 3)
+                    p `ptShouldBeAbout` Point (0, 10 + sqrt 3)
+
+            it "should calculate the intersection of lines perpendicular to the axes" $ let
+                line1 = Line (Point (1,2)) (Angle 0) -- y = 2 forall x
+                line2 = Line (Point (3,4)) (Angle (pi/2)) -- x = 3 forall y
+                p = intersection line1 line2
+                in
+                    p `ptShouldBeAbout` Point (3,2)
+                
+            it "should return the starting point of the first line if they are the same line (but offset in prameter)" $ let
+                line1 = Line (Point(1,1)) (Angle pi/4)
+                line2 = Line (Point(2,2)) (Angle pi/4)
+                p = intersection line1 line2
+                in
+                    p `ptShouldBeAbout` Point (1,1)
+                    
+            it "should do what ??? if they never intersect? I guess we neeed a predicate method for that?" $
+                pending --Maybe we need to return a Maybe Point?
